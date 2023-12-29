586c6dcd103ff49cd5b712a3203156b90f2edf0d
AbdelillahElasri1 <elasriabdelilah68@gmail.com>
2023-12-29 12:23:31 +0100

    add structure project job\offre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "/jobOffre"},
+  {path: "/company"},
+  {path: "/agent"},
+  {path: "/"},
+  {path: "/application"},
+  {path: "/listOffre"}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
